80d1533a09bbd764ff51da0e12058359e04616c1
ryan.muktiadhi <ryan.muktiadhi@mii.co.id>
2025-09-11 15:36:14 +0700

    cleansing V.0.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,63 +43,6 @@ export const routes: Routes = [
        loadComponent:() => import("./pages/screen/tasklist/tasklist").then(m => m.Tasklist)
       },
       {
-       path:'shopee',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/inquery/inquery").then(m => m.Inquery)
-      },
-      {
-       path:'packaging',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/packaging/listinvoice/listinvoice").then(m => m.Listinvoice)
-      },
-      {
-        path: 'packaging/detail',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/screen/packaging/listinvoice/detailinvoice/detailinvoice').then(m => m.Detailinvoice),
-      },
-      {
-       path:'management/product',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/product/product.component").then(m => m.ProductComponent)
-      },
-      {
-       path:'management/warehouse',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/warehouselist/warehouselist").then(m => m.Warehouselist)
-      },
-      {
-        path:'management/stockopname',
-        canActivate:[authGuard],
-        loadComponent:() => import("./pages/screen/stockopname/stockopname").then(m => m.Stockopname)
-      },
-      {
-        path:'management/stockopname/view',
-        canActivate:[authGuard],
-        loadComponent:() => import("./pages/screen/stockopname/stockopnamedetail/stockopnamedetail").then(m => m.Stockopnamedetail),
-        data: { renderMode: 'server' }
-      },
-      {
-        path:'settings/shopee',
-        canActivate:[authGuard],
-        loadComponent:() => import("./pages/screen/shopeemanagement/shopeemanagement").then(m => m.Shopeemanagement),
-        data: { renderMode: 'server' }
-      },
-      {
-        path:'settings/user',
-        canActivate:[authGuard],
-        loadComponent:() => import("./pages/screen/usermanagement/usermanagement").then(m => m.Usermanagement),
-        data: { renderMode: 'server' }
-      },
-      {
-       path:'profile',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/myprofile/myprofile").then(m => m.Myprofile)
-      },
-      {
-       path:'printing',
-       loadComponent:() => import("./pages/screen/shopeeprintlist/shopeeprintlist").then(m => m.Shopeeprintlist)
-      },
-      {
         path: '',
         redirectTo: 'tasklist',
         pathMatch: 'full'
--- src/backend/routes/api.routes.ts
+++ src/backend/routes/api.routes.ts
@@ -7,20 +7,18 @@ import { asyncHandler } from '../middlewares/asyncHandler';
 const router = Router();
 router.get('/echo', echo);
 //##################################### REAL FUNCTION#############
-router.get('/shopee/get_qshopee', asyncHandler(authBearerMiddleware),asyncHandler(getQShopee)); // untuk menggenerate table q_shopee generateQShopeeJobs
-router.get('/shopee/get_qshopeetoday', asyncHandler(authBearerMiddleware),asyncHandler(getQShopeeToday)); // untuk melihat table q_shopee terakhir update per hari ini
-router.post('/shopee/gen_qshopee', asyncHandler(authBearerMiddleware),asyncHandler(generateQShopee)); // untuk menggenerate table q_shopee
-router.post('/shopee/gen_qshopeeCurrent', asyncHandler(authBearerMiddleware),asyncHandler(generateQShopeeCurrent)); // INI YANG POENTING
-router.post('/shopee/gen_qshopee_job', asyncHandler(authBearerMiddleware),asyncHandler(generateQShopeeJobs)); // untuk menggenerate table q_shopee_invoice
-router.post('/shopee/get_positem', asyncHandler(authBearerMiddleware),asyncHandler(viewQShopeePosItem)); // untuk menggenerate table q_shopee generateQShopeeJobs
-router.get('/shopee/get_shop_performance', asyncHandler(authBearerMiddleware),asyncHandler(getShopPerformance)); // untuk mendapatka performa toko terhadap shopee
-router.get('/shopee/get_shop_info', asyncHandler(authBearerMiddleware),asyncHandler(getShopInfo)); //mendapatkan Info toko dari Shopee
-router.post('/shopee/get_data_print', asyncHandler(authBearerMiddleware),asyncHandler(getBestDataToPrint));// Untuk print label invoice
-router.post('/shopee/send_print', asyncHandler(authBearerMiddleware),asyncHandler(sendingPrinting));// Untuk print label invoice
-
-router.get('/shopee/get_attributes', asyncHandler(authBearerMiddleware),asyncHandler(getQShopeeAttribute));
-router.post('/shopee/update_attributes', asyncHandler(authBearerMiddleware),asyncHandler(updateQShopeeAttribute));
-
+// router.get('/shopee/get_qshopee', asyncHandler(authBearerMiddleware),asyncHandler(getQShopee)); // untuk menggenerate table q_shopee generateQShopeeJobs
+// router.get('/shopee/get_qshopeetoday', asyncHandler(authBearerMiddleware),asyncHandler(getQShopeeToday)); // untuk melihat table q_shopee terakhir update per hari ini
+// router.post('/shopee/gen_qshopee', asyncHandler(authBearerMiddleware),asyncHandler(generateQShopee)); // untuk menggenerate table q_shopee
+// router.post('/shopee/gen_qshopeeCurrent', asyncHandler(authBearerMiddleware),asyncHandler(generateQShopeeCurrent)); // INI YANG POENTING
+// router.post('/shopee/gen_qshopee_job', asyncHandler(authBearerMiddleware),asyncHandler(generateQShopeeJobs)); // untuk menggenerate table q_shopee_invoice
+// router.post('/shopee/get_positem', asyncHandler(authBearerMiddleware),asyncHandler(viewQShopeePosItem)); // untuk menggenerate table q_shopee generateQShopeeJobs
+// router.get('/shopee/get_shop_performance', asyncHandler(authBearerMiddleware),asyncHandler(getShopPerformance)); // untuk mendapatka performa toko terhadap shopee
+// router.get('/shopee/get_shop_info', asyncHandler(authBearerMiddleware),asyncHandler(getShopInfo)); //mendapatkan Info toko dari Shopee
+// router.post('/shopee/get_data_print', asyncHandler(authBearerMiddleware),asyncHandler(getBestDataToPrint));// Untuk print label invoice
+// router.post('/shopee/send_print', asyncHandler(authBearerMiddleware),asyncHandler(sendingPrinting));// Untuk print label invoice
+// router.get('/shopee/get_attributes', asyncHandler(authBearerMiddleware),asyncHandler(getQShopeeAttribute));
+// router.post('/shopee/update_attributes', asyncHandler(authBearerMiddleware),asyncHandler(updateQShopeeAttribute));
 //##################################### AUTH ROUTES #############
 router.post('/auth/login', asyncHandler(login));
 router.post('/auth/registuser', asyncHandler(registUser));
@@ -28,27 +26,27 @@ router.get('/auth/attrb',asyncHandler(authBearerMiddleware), asyncHandler(attrb)
 router.post('/auth/updateuser', asyncHandler(authBearerMiddleware),asyncHandler(updateUser));
 //##################################### AUTH ROUTES #############
 
-router.get('/warehouse/get_jobs/available', asyncHandler(authBearerMiddleware),asyncHandler(viewQShopeePosItem));// Untuk mengambil Jobs / POS barang list dalam Inquery Shopee yang available
-router.get('/warehouse/get_packages', asyncHandler(authBearerMiddleware),asyncHandler(getPackageJobAvailable));// Untuk mengambil Packaging list dalam Jobs yang available
-router.post('/warehouse/check_taken_packages', asyncHandler(authBearerMiddleware),asyncHandler(checkPackageTaken));// Untuk cek apaka package yang di ambil sudah diambil user lain
-router.post('/warehouse/get_items_packages', asyncHandler(authBearerMiddleware),asyncHandler(getItemsInPackage)); // Untuk melihat Posisi banyaknya Item pada resi yang ada
-router.post('/warehouse/update_items_packages', asyncHandler(authBearerMiddleware),asyncHandler(updateItemsInPackage)); // Untuk mengupdate item yang sudah di kerjakan
-router.get('/warehouse/get_resi_count', asyncHandler(authBearerMiddleware),asyncHandler(getCountInvoicesAvailable)); // Untuk melihat jumlah total invoices belum di kerjakan
-router.get('/warehouse/get_sku_all', asyncHandler(authBearerMiddleware),asyncHandler(getAllSKUAvailable)); // Untuk melihat barang yang ada di stocks
-router.get('/warehouse/get_sku_count', asyncHandler(authBearerMiddleware),asyncHandler(getCountSKUAvailable)); // Untuk melihat jumlah total barang
-router.get('/warehouse/get_warehouse_all', asyncHandler(authBearerMiddleware),asyncHandler(getAllWarhouseAvailable)); // Untuk melihat warehouse yang ada
+// router.get('/warehouse/get_jobs/available', asyncHandler(authBearerMiddleware),asyncHandler(viewQShopeePosItem));// Untuk mengambil Jobs / POS barang list dalam Inquery Shopee yang available
+// router.get('/warehouse/get_packages', asyncHandler(authBearerMiddleware),asyncHandler(getPackageJobAvailable));// Untuk mengambil Packaging list dalam Jobs yang available
+// router.post('/warehouse/check_taken_packages', asyncHandler(authBearerMiddleware),asyncHandler(checkPackageTaken));// Untuk cek apaka package yang di ambil sudah diambil user lain
+// router.post('/warehouse/get_items_packages', asyncHandler(authBearerMiddleware),asyncHandler(getItemsInPackage)); // Untuk melihat Posisi banyaknya Item pada resi yang ada
+// router.post('/warehouse/update_items_packages', asyncHandler(authBearerMiddleware),asyncHandler(updateItemsInPackage)); // Untuk mengupdate item yang sudah di kerjakan
+// router.get('/warehouse/get_resi_count', asyncHandler(authBearerMiddleware),asyncHandler(getCountInvoicesAvailable)); // Untuk melihat jumlah total invoices belum di kerjakan
+// router.get('/warehouse/get_sku_all', asyncHandler(authBearerMiddleware),asyncHandler(getAllSKUAvailable)); // Untuk melihat barang yang ada di stocks
+// router.get('/warehouse/get_sku_count', asyncHandler(authBearerMiddleware),asyncHandler(getCountSKUAvailable)); // Untuk melihat jumlah total barang
+// router.get('/warehouse/get_warehouse_all', asyncHandler(authBearerMiddleware),asyncHandler(getAllWarhouseAvailable)); // Untuk melihat warehouse yang ada
 
-router.get('/warehouse/get_stockopname_all', asyncHandler(authBearerMiddleware),asyncHandler(getAllStockopname)); // Untuk melihat keseluruhan stock opname yang ada
-router.post('/warehouse/insert_stockopname', asyncHandler(authBearerMiddleware),asyncHandler(insertStockopname));
-router.post('/warehouse/update_stockopname', asyncHandler(authBearerMiddleware),asyncHandler(insertStockopname));
-router.post('/warehouse/delete_stockopname', asyncHandler(authBearerMiddleware),asyncHandler(insertStockopname));
-router.get('/warehouse/get_stocks_so', asyncHandler(authBearerMiddleware),asyncHandler(getAllSKUOnTransaction)); // Untuk melihat barang yang ada selama transaksi
-router.post('/warehouse/get_stockopnamedetail', asyncHandler(authBearerMiddleware),asyncHandler(getStockDetailopnameByIdOP));
-router.post('/warehouse/get_stockopnamedetailExport', asyncHandler(authBearerMiddleware),asyncHandler(getStockDetailopnameByIdOPExcel));
-router.post('/warehouse/insert_stockopnamedetail', asyncHandler(authBearerMiddleware),asyncHandler(insertStockDetailopname));
+// router.get('/warehouse/get_stockopname_all', asyncHandler(authBearerMiddleware),asyncHandler(getAllStockopname)); // Untuk melihat keseluruhan stock opname yang ada
+// router.post('/warehouse/insert_stockopname', asyncHandler(authBearerMiddleware),asyncHandler(insertStockopname));
+// router.post('/warehouse/update_stockopname', asyncHandler(authBearerMiddleware),asyncHandler(insertStockopname));
+// router.post('/warehouse/delete_stockopname', asyncHandler(authBearerMiddleware),asyncHandler(insertStockopname));
+// router.get('/warehouse/get_stocks_so', asyncHandler(authBearerMiddleware),asyncHandler(getAllSKUOnTransaction)); // Untuk melihat barang yang ada selama transaksi
+// router.post('/warehouse/get_stockopnamedetail', asyncHandler(authBearerMiddleware),asyncHandler(getStockDetailopnameByIdOP));
+// router.post('/warehouse/get_stockopnamedetailExport', asyncHandler(authBearerMiddleware),asyncHandler(getStockDetailopnameByIdOPExcel));
+// router.post('/warehouse/insert_stockopnamedetail', asyncHandler(authBearerMiddleware),asyncHandler(insertStockDetailopname));
 
 //##################################### EMAIL #############
-router.post('/warehouse/send_email', asyncHandler(sendingEmailTo));// Untuk cek apaka package yang di ambil sudah diambil user lain
+// router.post('/warehouse/send_email', asyncHandler(sendingEmailTo));// Untuk cek apaka package yang di ambil sudah diambil user lain
 //##################################### Printing #############
 
 export default router;
