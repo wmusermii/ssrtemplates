7f3f95522ff395a1d8257d1c323c5495d6d7c9e1
ryan.muktiadhi <ryan.muktiadhi@mii.co.id>
2025-10-04 15:37:23 +0700

    initial V.0.3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,63 +4,73 @@ import { authGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
   {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full'
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
   },
   {
-    path:'',
-    loadComponent:() => import("./layouts/navigations/nonavi/nonavi").then(m => m.Nonavi),
-    children:[
+    path: '',
+    loadComponent: () =>
+      import("./layouts/navigations/nonavi/nonavi").then(m => m.Nonavi),
+    children: [
       {
-       path:'login',
-       canActivate:[guestGuard],
-      loadComponent:() => import("./pages/screen/auths/login/login").then(m => m.Login)
+        path: 'login',
+        canActivate: [guestGuard],
+        loadComponent: () => import("./pages/screen/auths/login/login").then(m => m.Login),
+        data: { title: 'Login - Admin Template' }
       },
       {
-       path:'registration',
-      loadComponent:() => import("./pages/screen/auths/registration/registration").then(m => m.Registration)
+        path: 'registration',
+        loadComponent: () => import("./pages/screen/auths/registration/registration").then(m => m.Registration),
+        data: { title: 'Registration - Admin Template' }
       }
     ]
   },
   {
-    path:'',
-    loadComponent:() => import("./layouts/navigations/sidenavi/sidenavilayout/sidenavilayout").then(m => m.Sidenavilayout),
-    children:[
+    path: '',
+    loadComponent: () => import("./layouts/navigations/sidenavi/sidenavilayout/sidenavilayout").then(m => m.Sidenavilayout),
+    children: [
       {
-       path:'dashboard',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/dashboard/dashboard").then(m => m.Dashboard)
+        path: 'dashboard',
+        canActivate: [authGuard],
+        loadComponent: () => import("./pages/screen/dashboard/dashboard").then(m => m.Dashboard),
+        data: { title: 'Dashboard - Admin Template' }
       },
       {
-       path:'management/general',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/managements/generalmanagement/generalmanagement").then(m => m.Generalmanagement)
+        path: 'management/general',
+        canActivate: [authGuard],
+        loadComponent: () => import("./pages/screen/managements/generalmanagement/generalmanagement").then(m => m.Generalmanagement),
+        data: { title: 'General Management - Admin Template' }
       },
       {
-       path:'management/user',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/managements/usermanagement/usermanagement").then(m => m.Usermanagement)
+        path: 'management/user',
+        canActivate: [authGuard],
+        loadComponent: () => import("./pages/screen/managements/usermanagement/usermanagement").then(m => m.Usermanagement),
+        data: { title: 'User Management - Admin Template' }
       },
       {
-       path:'management/menus',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/managements/menumanagement/menumanagement").then(m => m.Menumanagement)
+        path: 'management/menus',
+        canActivate: [authGuard],
+        loadComponent: () => import("./pages/screen/managements/menumanagement/menumanagement").then(m => m.Menumanagement),
+        data: { title: 'Menu Management - Admin Template' }
       },
       {
-       path:'management/group',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/managements/groupmanagement/groupmanagement").then(m => m.Groupmanagement)
+        path: 'management/group',
+        canActivate: [authGuard],
+        loadComponent: () => import("./pages/screen/managements/groupmanagement/groupmanagement").then(m => m.Groupmanagement),
+        data: { title: 'Group Management - Admin Template' }
       },
       {
-       path:'management/role',
-       canActivate:[authGuard],
-       loadComponent:() => import("./pages/screen/managements/rolemanagement/rolemanagement").then(m => m.Rolemanagement)
+        path: 'management/role',
+        canActivate: [authGuard],
+        loadComponent: () => import("./pages/screen/managements/rolemanagement/rolemanagement").then(m => m.Rolemanagement),
+        data: { title: 'Role Management - Admin Template' }
       },
       {
-       path:'privacy/profile',
-       canActivate:[authGuard],
-      loadComponent:() => import("./pages/screen/myprofile/myprofile").then(m => m.Myprofile)
+        path: 'privacy/profile',
+        canActivate: [authGuard],
+        loadComponent: () => import("./pages/screen/myprofile/myprofile").then(m => m.Myprofile),
+        data: { title: 'My Profile - Admin Template' }
       }
     ]
   },
@@ -70,4 +80,3 @@ export const routes: Routes = [
     redirectTo: 'dashboard'
   }
 ];
-
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, signal, inject, effect } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd, ActivatedRoute } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map, mergeMap } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -8,5 +10,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ssrtemplates');
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  private titleService = inject(Title)
+  // protected readonly title = signal('ssrtemplates');
+  constructor() {
+    // listen ke perubahan route
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let route = this.activatedRoute.firstChild;
+        while (route?.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      mergeMap(route => route?.data ?? [])
+    ).subscribe(data => {
+      const newTitle = data['title'] ?? 'Web Admin';
+      this.titleService.setTitle(newTitle);
+    });
+  }
 }
